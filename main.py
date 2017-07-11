d1f286124c1003045c2910ebff90dd57efab1367
iMearner <201452060@iiitvadodara.ac.in>
2017-07-11 23:21:50 +0530

    some changes where made
    added some new conditions that escape code from stuck in the while loop

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
+import requests
+import sys
 
-f = open('untitled.html','r',encoding='utf-8') # to encode the file else it gives error 
+content = requests.get(str(sys.argv[1]))
+s = content.text
+
+print("reading data")
+
+newContent = open('untitled.html','w',encoding='utf-8') 
+newContent.write(s)  # python will convert \n to os.linesep
+newContent.close()
+images = list()
+
+print("data written successfully ")
 
 newLine = ''
 
-for line in f :
-	# print(line)
-	while ('<' in line and '>' in line ) :
+f = open('untitled.html','r',encoding='utf-8') 
 
-  		# checking index of < and > in string passed 
-		start = line.index('<')
-		end = line.index('>')
+images = list()
+
+newLine = ''
+for line in f :
+	while ('<' in line and '>' in line ) :
+		# print("loop is running ", end='\r')
+			# checking index of < and > in string passed 
 		
 
-		# print("loop is running ", end='\r')
+		start = line.index('<')
+		end = line.index('>')
 
-		subString = line[start:end+1]
+		# print("start %d and end is %d" %(start,end))
 
-		line = line.replace(subString,'')
-	if ('param' in line or  'it' in line and '=' not in line and '(' not in line ):
+		if end > start :
+			subString = line[start:end+1]
+			line = line.replace(subString,'')
+		if end < start :
+			line =  line[end+1:]
+			line.strip() 
+	if '<' in line or '>' in line or '{' in line or '[' in line or '(' in line or '&' in line or ';' in line or '}' in line or ']' in line or ')' in line :
+		pass
+	else:
+		line = line.replace('\n','')
+		line = line.replace('\t','')
 		newLine += line
 
 
-print(newLine.strip(' '))
+print(newLine.encode('utf-8'))
+
 
