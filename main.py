b9b0b298ca7375d72095b55d10c8ee3425534552
iMearner <201452060@iiitvadodara.ac.in>
2017-07-17 23:33:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,18 @@
-import requests
+from urllib2 import urlopen 
+from bs4 import BeautifulSoup
 import sys
+import re
 
-content = requests.get(str(sys.argv[1]))
-s = content.text
+url = str(sys.argv[1])
 
-print("reading data")
+html = urlopen(url)
 
-newContent = open('untitled.html','w',encoding='utf-8') 
-newContent.write(s)  # python will convert \n to os.linesep
-newContent.close()
-images = list()
+content = list()
+bs = BeautifulSoup(html,'lxml')
 
-print("data written successfully ")
+tag = bs.body
+raw_contents = tag.findAll('p')
 
-newLine = ''
-
-f = open('untitled.html','r',encoding='utf-8') 
-
-images = list()
-
-newLine = ''
-for line in f :
-	while ('<' in line and '>' in line ) :
-		# print("loop is running ", end='\r')
-			# checking index of < and > in string passed 
-		
-
-		start = line.index('<')
-		end = line.index('>')
-
-		# print("start %d and end is %d" %(start,end))
-
-		if end > start :
-			subString = line[start:end+1]
-			line = line.replace(subString,'')
-		if end < start :
-			line =  line[end+1:]
-			line.strip() 
-	if '<' in line or '>' in line or '{' in line or '[' in line or '(' in line or '&' in line or ';' in line or '}' in line or ']' in line or ')' in line :
-		pass
-	else:
-		line = line.replace('\n','')
-		line = line.replace('\t','')
-		newLine += line
-
-
-print(newLine.encode('utf-8'))
-
+for para in raw_contents :
+	print(para)
 
