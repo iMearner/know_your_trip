d2e9a56cd540df404597902df136e2493a718f59
iMearner <201452060@iiitvadodara.ac.in>
2017-07-18 20:54:32 +0530

    Update result.php

--- result.php
+++ result.php
@@ -98,7 +98,7 @@
 
 
 					
-					$output = shell_exec("python /home/sachin/Documents/main.py " . $link . " 2>&1");
+					$output = shell_exec("python3 /home/sachin/Documents/main.py " . $link . " 2>&1");
 					echo $output;
 				}				
 				?> 
