970d8792e6a8e3750ae0129dc86c9e5418ebfe5c
iMearner <201452060@iiitvadodara.ac.in>
2017-07-18 21:00:48 +0530

    successfully scraping content from urls

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-from urllib2 import urlopen 
+import requests 
 from bs4 import BeautifulSoup
 import sys
 import re
 
-url = str(sys.argv[1])
+url1= ''
+url2 = ''
 
-html = urlopen(url)
+q = str(sys.argv[1]) # query inserted by user 
 
-content = list()
-bs = BeautifulSoup(html,'lxml')
+query = q + "travel" + "tour" + "blog"   # string added to make result more relevant 
 
-tag = bs.body
-raw_contents = tag.findAll('p')
+url = "https://www.google.co.in/search?q=" + query # passing query to google search engine 
 
-for para in raw_contents :
-	print(para)
+html = requests.get(url) # get html content 
 
+data = html.text # display text of html 
+
+b_soup = BeautifulSoup(data,'lxml') # using  beautifulsoup to  fetch certain portion of web
+
+links = b_soup.findAll("h3",{"class":"r"}) # to get url of other
+												# website, scraping google page with specific class 
+
+# get url of website from the source code 
+for link in str(links[0]).split('"'):
+	if "http" in link :
+		if '=' in link:
+			for item in link.split('=') :
+				if 'http' in item :
+					link = item 
+					break
+		if '&' in link:
+			for item in link.split('&') :
+				if 'http' in item :
+					link = item
+					break
+		if 'http' in link and '&' not in link and '=' not in link :
+			url1 = link 
+	else:
+		pass		
+
+# get html content of webpage 
+details = requests.get(url1)
+details = details.text
+
+# display content of scrapped webpage
+new_soup = BeautifulSoup(details,'lxml')
+para = new_soup.findAll('p')
+print(str(para).encode('utf-8'))
